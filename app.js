926331349fa0cb5e399316673ea182e2d6f64876
kadir-zeren <k4dzeren@gmail.com>
2024-03-18 21:22:02 +0300

    clg

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Localde bulunan script calisti");
+
+alert("Bu bir alert dir");
+
+karar = confirm("Emiin misiniz E/H");
+console.log(karar);
+
+console.warn("Bu bir uyaridir");
+
+console.error("Eyvah hata olustu..");
+
+console.log("APP JS RUNNIG");
+
+const pi = 3.14;
+console.log(pi);

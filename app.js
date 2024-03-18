10a7681b0a05b8f84a56de42e3fc1d7478650ccb
kadir-zeren <k4dzeren@gmail.com>
2024-03-18 21:30:00 +0300

    block scope

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
-console.log("Localde bulunan script calisti");
+// console.log("Localde bulunan script calisti");
 
-alert("Bu bir alert dir");
+// alert("Bu bir alert dir");
 
-karar = confirm("Emiin misiniz E/H");
-console.log(karar);
+// karar = confirm("Emiin misiniz E/H");
+// console.log(karar);
 
-console.warn("Bu bir uyaridir");
+// console.warn("Bu bir uyaridir");
 
-console.error("Eyvah hata olustu..");
+// console.error("Eyvah hata olustu..");
 
 console.log("APP JS RUNNIG");
 
 const pi = 3.14;
 console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+const number1 = 5;
+// number1 = 7;
+
+const varMi = true;
+const isOpen = false;
+console.log(isOpen, typeof isOpen);
+
+let language = "Java";
+console.log(language, typeof language);
+
+language = "Javascript";
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+console.log(language);
+
+let adet;
+console.log(adet);
+
+adet = "23";
+console.log(typeof adet);
+
+{
+  let localDegisken = 5;
+  localDegisken = localDegisken + 1;
+  console.log(localDegisken);
+}
+
